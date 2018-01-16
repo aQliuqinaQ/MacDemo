2bf4190508bc5c7c15c21c3aa6c766a353dbcd30
liuqin <liuqin@liuqin.local>
2018-01-16 10:01:59 +0800

    Initial Commit

---
+++ MacDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MacDemo
+//
+//  Created by liuqin on 2018/1/16.
+//  Copyright © 2018年 liuqin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
